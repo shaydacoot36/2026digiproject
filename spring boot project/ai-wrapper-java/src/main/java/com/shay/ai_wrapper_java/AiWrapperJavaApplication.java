85ac0d7b64148744081ee6449e4fc2e71b535734
shaydacoot36 <kueegrl@gmail.com>
2026-03-02 15:07:23 +1300

    spring boot test

---
+++ spring boot project/ai-wrapper-java/src/main/java/com/shay/ai_wrapper_java/AiWrapperJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shay.ai_wrapper_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWrapperJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWrapperJavaApplication.class, args);
+	}
+
+}
---
+++ spring boot project/ai-wrapper-java/src/test/java/com/shay/ai_wrapper_java/AiWrapperJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shay.ai_wrapper_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWrapperJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
